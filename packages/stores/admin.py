07170d33d4a6b80054448fb8d627e6733ab1c8e0
rtorres90 <roberto1990z>
2016-02-12 23:51:15 -0300

    adding admin and names easy to read

--- packages/stores/admin.py
+++ packages/stores/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from models import EmployeeTitle, Store
 
-admin.site.register(Store)
+admin.site.register((Store, EmployeeTitle))
--- packages/stores/models.py
+++ packages/stores/models.py
@@ -8,9 +8,15 @@ from django.db import models
 class Store(models.Model):
     name = models.CharField(max_length=20)
     location = models.CharField(max_length=50)
-    
+
+    def __unicode__(self):
+        return "%s[%s]" % (self.name, self.id)
+
+
 class EmployeeTitle(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=20)
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
-    
+
+    def __unicode__(self):
+        return "%s[%s]" % (self.title, self.id)
